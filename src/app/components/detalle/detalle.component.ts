fac409a7c675c5dd64e53654d6addbcbe03fc709
Andres Chavez <andres.cz.chavez@gmail.com>
2022-01-29 12:08:38 -0500

    Pantalla de detalle con botón de regresar y manejo de actores

--- src/app/components/detalle/detalle.component.ts
+++ src/app/components/detalle/detalle.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MoviesService } from '../../services/movies.service';
-import { PeliculaDetalle } from '../../models/intefaces/interfaces';
+import { Cast, PeliculaDetalle } from '../../models/intefaces/interfaces';
+import { SwiperOptions } from 'swiper';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-detalle',
@@ -10,14 +12,32 @@ import { PeliculaDetalle } from '../../models/intefaces/interfaces';
 export class DetalleComponent implements OnInit {
   @Input() id: string;
   pelicula: PeliculaDetalle = {};
-  constructor(private movieService: MoviesService) { }
+  oculto = 150;
+  actores: Cast[] = [];
+  config: SwiperOptions = {
+    slidesPerView: 2.4,
+    spaceBetween: -5,
+    freeMode: true,
+    pagination: { clickable: true },
+    scrollbar: { draggable: true, },
+    fadeEffect: {crossFade: true}
+  };
+  constructor(private movieService: MoviesService, private modalController: ModalController) { }
 
   ngOnInit() {
     this.movieService.getMovieDetalle(this.id).subscribe(data => {
       this.pelicula = data;
     });
     this.movieService.getActorePelicula(this.id).subscribe(data => {
+      this.actores = data.cast;
     });
   }
 
+
+  regresar(){
+    this.modalController.dismiss();
+  }
+  favorito(){
+
+  }
 }
