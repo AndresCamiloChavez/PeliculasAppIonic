379af5debec8bc0573b1b3fb09bd122a949e121d
Andres Chavez <andres.cz.chavez@gmail.com>
2022-01-29 12:51:25 -0500

    Tab de búsqueda peliculas

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
+  textoBuscar = '';
+  ideas: string[] =['Spiderman', 'Superman', 'Avenger', 'El señor de los añillos', 'Flash'];
   constructor() {}
-
+  buscar(event){
+    console.log('buscar -> ', event.detail.value);
+  }
 }
